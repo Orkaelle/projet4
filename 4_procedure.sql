15ac02d1a475a1a37531ebf97d19ce17b437163d
Orkaëlle <orkaelle.hardelin@gmail.com>
2020-09-01 11:59:50 +0200

    [MAJ] Bornage pop et area selon min et max + ajout calcul densité

--- 4_procedure.sql
+++ 4_procedure.sql
@@ -2,12 +2,27 @@ CREATE OR REPLACE PROCEDURE new (name TEXT)
 LANGUAGE plpgsql
 AS
 $$
+DECLARE 
+    maxpop int;
+    minpop int;
+    pop int;
+    maxarea int;
+    minarea int;
+    area int;
+    density int;
 BEGIN
+SELECT MAX(country.pop) INTO maxpop FROM country;
+SELECT MIN(country.pop) INTO minpop FROM country;
+SELECT MAX(country.land_area) INTO maxarea FROM country;
+SELECT MIN(country.land_area) INTO minarea FROM country;
+pop := floor(random() * ((maxpop - minpop)+1) + minpop);
+area := floor(random() * ((maxarea - minarea)+1) + minarea);
+density := pop/area;
 INSERT INTO country VALUES (
     name,
-    floor(random() * (999990000+1) + 10000)::int,
-    floor(random() * (999+1) + 1)::int,
-    floor(random() * (999990+1) + 10)::int
+    pop,
+    density,
+    area
 );
 END;
 $$
